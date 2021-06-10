244ee5b2ff338d77f8b0239c281cb58850b87273
Md Abu Zafar <mazafar.bd@gmail.com>
2021-06-10 08:47:57 +0600

    Add project files.

---
+++ src/Producer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+namespace Producer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "Producer";
+            var config1 = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                //SecurityProtocol = SecurityProtocol.Plaintext
+            };
+            var employee = new
+            {
+                Id = 1,
+                Name = "Lafiz"
+            };
+
+            string serializedEmployee = JsonConvert.SerializeObject(employee);
+            using var producer = new ProducerBuilder<Null, string>(config1).Build();
+            await producer.ProduceAsync("temp-topic-with-t3", new Message<Null, string> { Value = serializedEmployee });
+            //producer.Flush(TimeSpan.FromSeconds(10));
+            producer.Flush();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Subscriber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Consumer";
+            var config = new ConsumerConfig
+            {
+                GroupId = "gid-consumers-t",
+                BootstrapServers = "localhost:9092"
+            };
+
+            using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+            consumer.Subscribe("temp-topic-with-t3");
+            while (true)
+            {
+                var cr = consumer.Consume();
+                Console.WriteLine(cr.Message.Value);
+            }
+        }
+    }
+}
